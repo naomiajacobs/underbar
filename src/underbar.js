ac60b08a40a40113151f003053392edc6c6e1cab
naomiajacobs <naomiajacobs@gmail.com>
2015-07-03 16:06:18 -0400

    Made filter tests pass

--- src/underbar.js
+++ src/underbar.js
@@ -83,6 +83,15 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var filtered = [];
+
+    _.each(collection, function(item, index) {
+      if (test(item)) {
+        filtered.push(collection[index]);
+      }
+    });
+
+    return filtered;
   };
 
   // Return all elements of an array that don't pass a truth test.
