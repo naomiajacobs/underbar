f146b09e976fb617f51dbf416b2db7d5227d8263
naomiajacobs <naomiajacobs@gmail.com>
2015-07-03 15:29:05 -0400

    Made _.last tests pass

--- src/underbar.js
+++ src/underbar.js
@@ -38,6 +38,16 @@
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if (n === 0) {
+      return [];
+    }
+    else if (n > array.length) {
+      return array;
+    }
+    else
+    {
+      return n === undefined ? array[array.length-1] : array.slice(n-1, array.length);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
